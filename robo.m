1b99fed4805735ea409c7413eb6df628be9ea8ca
RodsCoimbra <rodscoimbra@gmail.com>
2024-12-08 23:45:40 +0000

    final

--- robo.m
+++ robo.m
@@ -1,21 +1,12 @@
 clear
 close all
-% dhparams = [0.05   pi/2   0.3585    0;       % Base
-%             0.3    0      -0.035    0;       % Shoulder
-%             0.35    0      0         0;       % Elbow
-%             0.251   0   0           0;       % Wrist Pitch
-%             0.1      -pi/2      0    0];      % Wrist Roll
-
+% a alpha d theta
 dhparams = [0.05   pi/2   0.3585   0;       % Base
             0.3   0      -0.035  0;       % Shoulder
             0.35    0     0          0;       % Elbow
             0.251  -pi/2  0         0;       % Wrist Pitch
             0     0      0    0];      % Wrist Roll
 
-
-% a alpha d theta
-
-% red - x, y - green, blue z
 robot = rigidBodyTree;
 num_joints = 5;
 bodies = cell(num_joints,1);
@@ -32,18 +23,16 @@ for i = 1:num_joints
     end
 end
 
-% show(robot);                
+figure(Name=['Initial' num2str(i)])
+show(robot)
 
-for i = num_joints:-1:1
-    figure(Name=['Initial' num2str(i)])
-    config = homeConfiguration(robot); % Retrieve current joint configurations
-    config(i).JointPosition = 0; % Set last joint to rotate by pi/4
-    show(robot, config); % Display the robot with the updated configuration
-
-
-
-    figure(Name=['Moved' num2str(i)])
-    config = homeConfiguration(robot); % Retrieve current joint configurations
-    config(i).JointPosition = pi/4; % Set last joint to rotate by pi/4
-    show(robot, config); % Display the robot with the updated configuration
-end
+figure(Name=['Moved' num2str(i)])
+config = homeConfiguration(robot);
+config(1).JointPosition = pi/4; 
+config(2).JointPosition = pi/2.4;
+config(3).JointPosition = -pi/1.6;
+config(4).JointPosition = pi/4;
+show(robot, config); 
+xlim([-0.5, 1])
+ylim([-0.5, 1])
+zlim([0, 1])
