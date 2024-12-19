f0c931b463988246e009f3c72911d761e68c728d
RodsCoimbra <rodscoimbra@gmail.com>
2024-12-19 10:58:30 +0000

    Tive de mudar o simulador, ver como seguir

--- Lab2/Lab.py
+++ Lab2/Lab.py
@@ -6,17 +6,16 @@ from collections import deque
 
 # Constants
 DELTA_T = 0.1  # Time interval in seconds
-ANGLE_INCREMENT = np.radians(60)  # Angle increment in radians
-MAX_ANGLE = np.radians(60)  # Maximum steering angle in radians
+ANGLE_INCREMENT = np.radians(6)  # Angle increment in radians
+MAX_ANGLE = np.radians(30)  # Maximum steering angle in radians
 WHEELBASE = 2.36  # Wheelbase in meters
 FRONT_WIDTH = 1.35 # Front wheel width in meters
 WHEEL_RADIUS = 0.330  # Wheel radius in meters
 
 class Car:
     def __init__(self, initial_position, velocity, wheelbase, front_width, wheel_radius):
-        self.car_position = deque([initial_position], 50)  # Keep only the last 50 measurements
-        self.time = deque([0], 50)
-        
+        self.car_position = deque([[initial_position, initial_position]], 30)  # Keep only the last 50 measurements
+        #self.time = deque([0], 50)
         self.wheel_radius = wheel_radius
         self.front_width = front_width
     
@@ -28,19 +27,18 @@ class Car:
         
         plt.ion() 
         _, self.ax = plt.subplots()
-        self.last_update_time = time.time()  # Track last update time
+        self.last_update_time = time.time()  
     
     def joystick(self, stdscr):
         curses.cbreak()
         stdscr.nodelay(True)  # Make getch non-blocking
         
         while True:
-            key = stdscr.getch()  # Get keyboard input (non-blocking)
-            
+            key = stdscr.getch()  
             if key == curses.KEY_LEFT:
-                self.change_angle(-ANGLE_INCREMENT)
-            elif key == curses.KEY_RIGHT:
                 self.change_angle(ANGLE_INCREMENT)
+            elif key == curses.KEY_RIGHT:
+                self.change_angle(-ANGLE_INCREMENT)
             elif key == ord('x'):
                 stdscr.addstr("Exiting...\n")
                 plt.ioff()
@@ -52,7 +50,6 @@ class Car:
             if current_time - self.last_update_time >= DELTA_T:
                 self.update_car_position()
                 self.last_update_time = current_time
-                self.time.append(self.time[-1] + DELTA_T)  # Simulate elapsed time
                 self.plot_car()
         
     def change_angle(self, angle):
@@ -62,17 +59,20 @@ class Car:
         self.phi += self.steering_angle - last_steering_angle
                 
     def update_car_position(self):
-        #delta_x = self.velocity * np.cos(self.theta) * np.cos(self.phi)* DELTA_T
+        delta_x = self.velocity * np.cos(self.theta) * np.cos(self.phi)* DELTA_T
         delta_y = self.velocity * np.sin(self.theta) * np.cos(self.phi) * DELTA_T
         self.theta += self.velocity * np.sin(self.phi)/self.wheelbase * DELTA_T  
-        self.car_position.append(self.car_position[-1] + delta_y)
-        
+        self.car_position.append([self.car_position[-1][0] + delta_x, self.car_position[-1][1] + delta_y])
+    
     def plot_car(self):
-        # Clear and redraw the plot
         self.ax.clear()
-        self.ax.plot(self.car_position, self.time, label='Steering Angle')
-        self.ax.plot(np.ones(len(self.time)) * 3.5,self.time, 'k--', label='Lane Limit')
-        self.ax.plot(np.ones(len(self.time)) * -3.5, self.time, 'k--')
+        car = np.array(self.car_position)
+        self.ax.plot(car[:,0], car[:,1], label='Car Position')
+        self.ax.scatter(self.car_position[-1][0], self.car_position[-1][1], c='r', label='Current Position')
+        # Create a square of 100x100 meters
+        self.ax.plot([-50, 50, 50, -50, -50], [-50, -50, 50, 50, -50], 'k--', label='Lane Limit')
+        # self.ax.plot(np.ones(len(self.time)) * 3.5,self.time, 'k--', label='Lane Limit')
+        # self.ax.plot(np.ones(len(self.time)) * -3.5, self.time, 'k--')
         self.ax.set_ylabel('Time Step (s)')
         self.ax.set_xlabel('Steering Angle (radians)')
         self.ax.legend()
@@ -81,5 +81,5 @@ class Car:
         plt.pause(0.001)
 
 if __name__ == "__main__":
-    car = Car(0, WHEELBASE, 15, FRONT_WIDTH, WHEEL_RADIUS)        
+    car = Car(0, 10,WHEELBASE, FRONT_WIDTH, WHEEL_RADIUS)        
     curses.wrapper(car.joystick)
